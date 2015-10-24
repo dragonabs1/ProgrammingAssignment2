bce05bb52950c99220d258810bc04654400f6bb3
dragon <dragonabs1@gmail.com>
2015-10-23 23:08:24 -0400

    Edited R file for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a matrix and caches its inverse
 
-## Write a short comment describing this function
+## Creates a list of functions to set and get input matrix and set and get its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inM = matrix()) {
+      i <- NULL  
+      setmat <- function(y) {
+            inM <<- y                            ## Sets input matrix to y and caches it.
+            i <<- NULL                           ## Initializes matrix to store inverse.
+      }
+      getmat <- function() inM                   ## getmat retrieves input matrix.
+      setinv <- function(inv) i <<- inv          ## setinv caches inverse matrix.
+      getinv <- function() i                     ## getinv retrieves (possibly empty) inverse matrix.
+      list(setmat = setmat, getmat = getmat,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Computes inverse of matrix object created by makeCacheMatrix, if inverse not already
+exists in cache; else return cached inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      i <- x$getinv()                        ## Retrieves (possibly empty) inverse matrix.
+      if(!is.null(i)) {                      ## Checks if retrieved inverse matrix is empty.
+            message("getting cached data")
+            return(i)                        ## If not, then it's from cache and is returned.
+      }
+      data <- x$getmat()                     ## Else input matrix is retrieved and its inverse compputed.
+      i <- solve(data, ...)
+      x$setinv(i)                            ## Computed inverse is cached and displayed.
+      i
 }
